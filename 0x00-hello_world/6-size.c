bb38f99b0b434ad8e011c5a22907330881e62a27
mingift <olisegft@gmail.com>
2022-12-08 19:37:39 -0800

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry 
+ * main - Entry point
  *
  * Return: Always 0
  */
@@ -11,5 +11,6 @@ int main(void)
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	printf("Size of an Str: %i byte(s)\n", sizzof(str));
 	return (0);
 }
