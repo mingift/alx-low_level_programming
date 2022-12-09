baf0fab8997ab9f8a1d56fae275377a3a6683bd9
mingift <olisegft@gmail.com>
2022-12-08 19:52:57 -0800

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-    #include <unistd.h>                                                                                                                      
-/** 
+#include <unistd.h>                                                                                 /** 
  * main - Entry
+ *
  * Return: Always 1
 */
 int main(void)
