585e6a01c28ef76c36f9cb45c7ab8d1f6ef5eb91
mingift <olisegft@gmail.com>
2022-12-08 17:47:03 -0800

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,12 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- * main - prints a String to the Standard error
- *
- * Return: always 1 (error)
+ * Return: Always 1
  */
-
 int main(void)
 {	
-	Write (2, " and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-	return(1);
+	Write (1, " and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Return: always 0 (success)
  */
-
 int main(void)
-{	printf(" Size of a Char: % byte(s)\n", Sizeof(char));
-	printf(" Size of an int: % byte(s)\n", Sizeof(int));
-	printf(" Size of a long int: % byte(s)\n", Sizeof(longint));
-	printf(" Size of a long long int: % byte(s)\n", Sizeof(longlongint));
-	printf(" Size of a float: % byte(s)\n", Sizeof(float));
-	return(0);
+{	printf(" Size of a Char: %i byte(s)\n", Sizeof(char));
+	printf(" Size of an int: %i byte(s)\n", Sizeof(int));
+	printf(" Size of a long int: %i byte(s)\n", Sizeof(long int));
+	printf(" Size of a long long int: %i byte(s)\n", Sizeof(long long int));
+	printf(" Size of a float: %i byte(s)\n", Sizeof(float));
+	return (0);
 }
