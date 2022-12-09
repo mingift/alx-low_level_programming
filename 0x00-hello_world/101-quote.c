6475cb29394b4e41dbefe06cb43448a6c41999fb
mingift <olisegft@gmail.com>
2022-12-08 18:40:16 -0800

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  * Return: Always 1
  */
 int main(void)
-{	
-	Write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59);
+{	Write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59);
 	return (1);
 }
