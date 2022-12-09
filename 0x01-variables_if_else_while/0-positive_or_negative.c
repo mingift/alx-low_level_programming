e64e3f1d5169527a04a56f1c004ede7d85b54322
mingift <olisegft@gmail.com>
2022-12-09 11:41:51 -0800

    Postive, negative ir zero numbers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Determine  if a number is positive, negative or zero
+ *
+ * Return: Alawys 0 (Success)
+ */
+int main(void)
+{	int n;
+	Srand(time(0));
+	n = rand(0) - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
