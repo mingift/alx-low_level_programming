876b4252237f3c9d70bbff7b93dc1b5582fafa83
mingift <olisegft@gmail.com>
2022-12-14 12:33:54 -0800

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -23,7 +23,7 @@ int main(void)
 
 	for (i = 1; i < 91; i++)
 	{
-		printf(",  %lu", aft);
+		printf(",  %lu%lu", aft);
 		aft += bef;
 		bef = aft - bef;
 	}
@@ -34,7 +34,7 @@ int main(void)
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(",  %lu", aft1 + (aft2 / l));
+		printf(", %lu%lu", aft1 + (aft2 / l));
 		printf("%lu", aft2 % l);
 		aft1 = aft1 + bef1;
 		bef1 = aft1 - bef1;
