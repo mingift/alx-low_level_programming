a742ecd01e3e6c3e7c94510871c171e1187e5161
mingift <olisegft@gmail.com>
2022-12-13 18:27:17 -0800

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,9 +15,9 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				printf("%d,", i);
+				printf("%d, ", i);
 			else if (i == 98)
-				printf("%d\n", i);
+				printf("%d\n", j);
 		}
 	}
 	else if (n >= 98)
@@ -25,7 +25,7 @@ void print_to_98(int n)
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-				printf("%d,", j);
+				printf("%d, ", j);
 			else if (j == 98)
 				printf("%d\n", j);
 		}
